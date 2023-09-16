fbacf20b851a6e0f405a25f332044f320a76d465
niyioo <niyiolagbegi@gmail.com>
2023-09-16 10:55:11 +0100

    updated place city and user

--- models/place.py
+++ models/place.py
@@ -1,18 +1,47 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class Place(BaseModel):
     """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+    __tablename__ = "places"
+
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024))
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        reviews = relationship("Review", backref="place", cascade="all, delete-orphan")
+        amenities = relationship("Amenity", secondary="place_amenity",
+                                viewonly=False)
+    else:
+        @property
+        def reviews(self):
+            """getter for reviews"""
+            review_dict = models.storage.all(Review)
+            place_reviews = []
+            for review in review_dict.values():
+                if review.place_id == self.id:
+                    place_reviews.append(review)
+            return place_reviews
+
+        @property
+        def amenities(self):
+            """getter for amenities"""
+            amenity_dict = models.storage.all(Amenity)
+            place_amenities = []
+            for amenity in amenity_dict.values():
+                if amenity.id in self.amenity_ids:
+                    place_amenities.append(amenity)
+            return place_amenities
--- models/user.py
+++ models/user.py
@@ -2,12 +2,18 @@
 """This module defines a class User"""
 from models.base_model import BaseModel
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class User(BaseModel):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
+
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        places = relationship("Place", backref="user", cascade="all, delete-orphan")
