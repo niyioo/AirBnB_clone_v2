2190aef1b0155c9d378e930d89c5771a5d886fe7
niyioo <niyiolagbegi@gmail.com>
2023-09-19 15:17:47 +0100

    fix

--- models/user.py
+++ models/user.py
@@ -10,13 +10,13 @@ class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
 
+    id = Column(Integer, primary_key=True, nullable=False,
+                unique=True, autoincrement=True)
+
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-
-    id = Column(Integer, primary_key=True, nullable=False,
-                unique=True, autoincrement=True)
 
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         places = relationship("Place", backref="user",
