88d6fbfff63dda987cb46b9b38df861c822daf57
niyioo <niyiolagbegi@gmail.com>
2023-09-19 13:29:32 +0100

    nn

--- models/base_model.py
+++ models/base_model.py
@@ -18,7 +18,6 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """initializes all attributes for SQLAlchemy
         """
-        from models import storage
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.utcnow()
