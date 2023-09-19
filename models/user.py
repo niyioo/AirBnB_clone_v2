b339d4feddb8a34d88f28106a756e7e9494a0d26
niyioo <niyiolagbegi@gmail.com>
2023-09-19 15:27:44 +0100

    fx

--- models/user.py
+++ models/user.py
@@ -15,6 +15,9 @@ class User(BaseModel, Base):
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
 
+    id = Column(Integer, primary_key=True, nullable=False,
+                unique=True, autoincrement=True)
+
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         places = relationship("Place", backref="user",
                               cascade="all, delete-orphan")
