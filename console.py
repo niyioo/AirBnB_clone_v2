ea947c85f2cdbcdd9f821f6eab519a1a2b2ac7cc
niyioo <niyiolagbegi@gmail.com>
2023-09-20 14:41:41 +0100

    nnnn

--- console.py
+++ console.py
@@ -231,23 +231,26 @@ class HBNBCommand(cmd.Cmd):
         print("Destroys an individual instance of a class")
         print("[Usage]: destroy <className> <objectId>\n")
 
-    def do_all(self, args):
-        """ Shows all objects, or all objects of a class"""
-        print_list = []
+    def do_all(self, line):
+        """Prints all string representation of all instances based on the class name.
+        Usage: all <class_name> or all
+        """
+        args = line.split()
+        objects = models.storage.all()
 
-        if args:
-            args = args.split(' ')[0]  # remove possible trailing args
-            if args not in HBNBCommand.classes:
-                print("** class doesn't exist **")
-                return
-            for k, v in storage._FileStorage__objects.items():
-                if k.split('.')[0] == args:
-                    print_list.append(str(v))
-        else:
-            for k, v in storage._FileStorage__objects.items():
-                print_list.append(str(v))
+        if not line:
+            print(objects)
+            return
 
-        print(print_list)
+        if args[0] not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+
+        class_name = HBNBCommand.classes[args[0]]
+
+        obj_list = [str(obj) for obj in objects.values()
+                    if isinstance(obj, class_name)]
+        print(obj_list)
 
     def help_all(self):
         """ Help information for the all command """
