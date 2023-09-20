eaad91e9f40d8f00fe0c684732e07fd2c9df84e0
niyioo <niyiolagbegi@gmail.com>
2023-09-20 12:25:56 +0100

    x

--- models/place.py
+++ models/place.py
@@ -9,14 +9,15 @@ from sqlalchemy.sql import text
 
 metadata = Base.metadata
 
-place_amenity = Table(
-    'place_amenity',
-    metadata,
-    Column('place_id', String(60), ForeignKey(
-        'places.id'), primary_key=True, nullable=False),
-    Column('amenity_id', String(60), ForeignKey(
-        'amenities.id'), primary_key=True, nullable=False)
-)
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    place_amenity = Table(
+        'place_amenity',
+        metadata,
+        Column('place_id', String(60), ForeignKey(
+            'places.id'), primary_key=True, nullable=False),
+        Column('amenity_id', String(60), ForeignKey(
+            'amenities.id'), primary_key=True, nullable=False)
+    )
 
 
 class Place(BaseModel, Base):
