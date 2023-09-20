abf29bd1b3cea6c9767426e69ace11f3ce19cbc0
niyioo <niyiolagbegi@gmail.com>
2023-09-20 14:09:19 +0100

    nnn

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from datetime import datetime
+
+Base = declarative_base()
 
 
 class BaseModel:
--- models/user.py
+++ models/user.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from datetime import datetime
+
+Base = declarative_base()
 
 
 class User(BaseModel):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    __tablename__ = 'users'
+    id = Column(String(60), primary_key=True)
+    name = Column(String(128))
+    email = Column(String(128))
+    password = Column(String(128))
