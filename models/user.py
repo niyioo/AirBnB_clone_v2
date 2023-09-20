66c359b2257ff7526a3b4795093abe82cd8d2c16
niyioo <niyiolagbegi@gmail.com>
2023-09-20 11:52:32 +0100

    b

--- models/user.py
+++ models/user.py
@@ -9,7 +9,7 @@ from os import getenv
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = "users"
+        __tablename__ = 'users'
         id = Column(Integer, primary_key=True, nullable=False,
                     unique=True, autoincrement=True)
         email = Column(String(128), nullable=False)
