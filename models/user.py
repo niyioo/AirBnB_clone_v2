c877b1cc35dd323355a1ae33c8e1c133aeb6ecfa
niyioo <niyiolagbegi@gmail.com>
2023-09-20 15:08:48 +0100

    vvv

--- models/user.py
+++ models/user.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
-from sqlalchemy import Column, String, DateTime
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from datetime import datetime
-
-Base = declarative_base()
+from sqlalchemy import Column, String
 
 
 class User(BaseModel):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
-    id = Column(String(60), primary_key=True)
-    name = Column(String(128))
-    email = Column(String(128))
-    password = Column(String(128))
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
