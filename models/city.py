852b28c904c992d10c1de7a8e70c243194a879a1
niyioo <niyiolagbegi@gmail.com>
2023-09-21 02:47:28 +0100

    nnn

--- models/city.py
+++ models/city.py
@@ -3,20 +3,12 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-from os import getenv
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
-
+    __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    state = relationship("State", back_populates="cities")
-
-    def __init__(self, *args, **kwargs):
-        """Initialize City instance"""
-        super().__init__(*args, **kwargs)
-        if getenv('HBNB_TYPE_STORAGE') != 'db':
-            self.state_id = ""
-            self.name = ""
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="cities")
