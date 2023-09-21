968ac0fd45141bee3a74aea1a6fbad7ece43eae4
niyioo <niyiolagbegi@gmail.com>
2023-09-21 01:52:08 +0100

    fx

--- models/city.py
+++ models/city.py
@@ -8,13 +8,12 @@ from os import getenv
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'cities'
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        state = relationship("State", back_populates="cities")
-        places = relationship('Place', backref='cities',
-                              cascade='all, delete-orphan')
-    else:
+    __tablename__ = 'cities'
+
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    state = relationship("State", back_populates="cities")
+
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
         state_id = ""
         name = ""
