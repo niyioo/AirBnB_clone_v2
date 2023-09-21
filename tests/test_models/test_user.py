8c70fe2eeec7d3a4f0a7d3434458e01621b63072
niyioo <niyiolagbegi@gmail.com>
2023-09-21 04:10:07 +0100

    cc

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Unit tests for User class"""
 import unittest
-from tests.test_models.test_base_model import test_basemodel
+from tests.test_models.test_base_model import TestBaseModel
 from models.user import User
 
 
