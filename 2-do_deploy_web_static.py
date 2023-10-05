eed90fb4957cd7fd109e6bbd53c46bcbc36a8740
niyioo <niyiolagbegi@gmail.com>
2023-10-05 04:28:42 -0700

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,11 +2,11 @@
 """
 This Fabric script deploys an archive to web servers.
 """
-from fabric.api import env, put, run, sudo
-import os
+
+from fabric.api import env, put, run
+import os.path
 
 # Specify the SSH user and web server IP addresses
-env.user = 'ubuntu'
 env.hosts = ['54.236.30.173', '100.26.56.25']
 
 
@@ -20,37 +20,50 @@ def do_deploy(archive_path):
     Returns:
         True if all operations have been done correctly, otherwise False.
     """
-    if not os.path.exists(archive_path):
+    # Check if the archive file exists
+    if os.path.isfile(archive_path) is False:
         return False
 
-    # Extract archive filename without extension
-    archive_filename = os.path.basename(archive_path).split('.')[0]
+    # Extract the filename and name (without extension) from the archive path
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
 
     # Upload the archive to /tmp/ on the web server
-    print('Uploading archive...')
-    put(archive_path, '/tmp/')
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+        return False
 
-    # Create the release folder if it doesn't exist
-    print('Creating release folder...')
-    sudo('mkdir -p /data/web_static/releases/{}'.format(archive_filename))
+    # Remove the existing release folder
+    if run("rm -rf /data/web_static/releases/{}/".format(name)).failed is True:
+        return False
 
-    # Uncompress the archive to /data/web_static/releases/
-    print('Uncompressing archive...')
-    sudo('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format(
-        archive_filename + '.tgz', archive_filename))
+    # Create the release folder
+    if run("mkdir -p /data/web_static/releases/{}/".format(name)).failed is True:
+        return False
 
-    # Delete the uploaded archive
-    print('Deleting uploaded archive...')
-    sudo('rm /tmp/{}'.format(archive_filename + '.tgz'))
+    # Uncompress the archive into the release folder
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(file, name)).failed is True:
+        return False
 
-    # Delete the existing symbolic link
-    print('Deleting existing symbolic link...')
-    sudo('rm /data/web_static/current')
+    # Remove the uploaded archive
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
 
-    # Create a new symbolic link to the new version
-    print('Creating a new symbolic link...')
-    sudo('ln -s /data/web_static/releases/{} /data/web_static/current'.format(
-        archive_filename))
+    # Move the contents of the release folder
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
 
-    print('New version deployed!')
+    # Remove the redundant web_static subfolder
+    if run("rm -rf /data/web_static/releases/{}/web_static".format(name)).failed is True:
+        return False
+
+    # Remove the existing current symlink
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+
+    # Create a new symlink to the latest release
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(name)).failed is True:
+        return False
+
+    # Return True if all operations were successful
     return True
