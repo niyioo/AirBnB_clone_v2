a9c8699ce8ff4ad67974b4315f6ad63e966158b5
niyioo <niyiolagbegi@gmail.com>
2023-10-05 04:04:25 -0700

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,7 +7,7 @@ import os
 
 # Specify the SSH user and web server IP addresses
 env.user = 'ubuntu'
-env.hosts = ['100.26.56.25', '54.89.25.134']
+env.hosts = ['54.236.30.173', '100.26.56.25']
 
 
 def do_deploy(archive_path):
@@ -27,23 +27,30 @@ def do_deploy(archive_path):
     archive_filename = os.path.basename(archive_path).split('.')[0]
 
     # Upload the archive to /tmp/ on the web server
+    print('Uploading archive...')
     put(archive_path, '/tmp/')
 
     # Create the release folder if it doesn't exist
+    print('Creating release folder...')
     sudo('mkdir -p /data/web_static/releases/{}'.format(archive_filename))
 
     # Uncompress the archive to /data/web_static/releases/
+    print('Uncompressing archive...')
     sudo('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format(
         archive_filename + '.tgz', archive_filename))
 
     # Delete the uploaded archive
+    print('Deleting uploaded archive...')
     sudo('rm /tmp/{}'.format(archive_filename + '.tgz'))
 
     # Delete the existing symbolic link
+    print('Deleting existing symbolic link...')
     sudo('rm /data/web_static/current')
 
     # Create a new symbolic link to the new version
+    print('Creating a new symbolic link...')
     sudo('ln -s /data/web_static/releases/{} /data/web_static/current'.format(
         archive_filename))
 
+    print('New version deployed!')
     return True
