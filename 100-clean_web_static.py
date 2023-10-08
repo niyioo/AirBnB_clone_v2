66078443df811a52eb074b86d1d4c1f53b0e034d
niyioo <niyiolagbegi@gmail.com>
2023-10-07 20:35:31 -0700

    l

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,60 +1,124 @@
 #!/usr/bin/python3
 """
-This Fabric script cleans up old archives on web servers.
+This Fabric script creates and distributes an archive to web servers.
 """
-
-from fabric.api import env, local, run
-from os.path import exists
+import os
+import re
+from fabric.api import *
 from datetime import datetime
 
+env.user = "ubuntu"
 env.hosts = ["54.236.30.173", "100.26.56.25"]
+env.key_filename = "~/id_rsa"
+
+
+def do_pack():
+    """
+    Generate a .tgz archive of web_static and store it in the versions folder.
+
+    Returns:
+        Archive path if successful, None otherwise.
+    """
+    target = local("mkdir -p versions")
+    name = str(datetime.now()).replace(" ", '')
+    opt = re.sub(r'[^\w\s]', '', name)
+    tar = local('tar -cvzf versions/web_static_{}.tgz web_static'.format(opt))
+    if os.path.exists("./versions/web_static_{}.tgz".format(opt)):
+        return os.path.normpath("/versions/web_static_{}.tgz".format(opt))
+    else:
+        return None
+
+
+def do_deploy(archive_path):
+    """
+    Distribute an archive to web servers and update the current symlink.
+
+    Args:
+        archive_path (str): Path to the archive to deploy.
+
+    Returns:
+        True if all operations have been done correctly, otherwise False.
+    """
+    if not os.path.isfile(archive_path):
+        return False
+
+    # Extract the filename and name (without extension) from the archive path
+    file = os.path.basename(archive_path)
+    name = os.path.splitext(file)[0]
+
+    # Upload the archive to /tmp/ on the web server
+    put(archive_path, "/tmp/{}".format(file))
+
+    # Remove the existing release folder
+    run("rm -rf /data/web_static/releases/{}/".format(name))
+
+    # Create the release folder
+    run("mkdir -p /data/web_static/releases/{}/".format(name))
+
+    # Uncompress the archive into the release folder
+    run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(file, name))
+
+    # Remove the uploaded archive
+    run("rm /tmp/{}".format(file))
+
+    # Move the contents of the release folder
+    run("mv /data/web_static/releases/{}/web_static/* "
+        "/data/web_static/releases/{}/".format(name, name))
+
+    # Remove the redundant web_static subfolder
+    run("rm -rf /data/web_static/releases/{}/web_static".format(name))
+
+    # Remove the existing current symlink
+    run("rm -rf /data/web_static/current")
+
+    # Create a new symlink to the latest release
+    run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+        .format(name))
+
+    print("New version deployed!")
+    return True
+
+
+def deploy():
+    """
+    Deploy the web_static archive to web servers.
+
+    Returns:
+        True if the deployment is successful, otherwise False.
+    """
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
+
+    return do_deploy(archive_path)
 
 
 def do_clean(number=0):
     """
-    Delete outdated archives on both web servers.
+    Delete out-of-date archives from versions
+    and /data/web_static/releases folders.
 
     Args:
-        number (int): The number of archives to keep.
-
-    Returns:
-        None
+        number (int): The number of archives to keep,
+        including the most recent.
     """
-    if number == 0:
-        number = 1
     number = int(number)
-
-    # List all archives in the versions folder on the local machine
-    local_archives = local("ls -1t versions", capture=True)
-
-    # List all archives in the /data/web_static/releases folder on the remote servers
-    remote_archives = run("ls -1t /data/web_static/releases")
-
-    # Ensure that the archives on both local and remote are not empty
-    if local_archives and remote_archives:
-        local_archives = local_archives.split("\n")  # Convert to a list
-        remote_archives = remote_archives.split("\n")  # Convert to a list
-
-        # Keep only the specified number of archives
-        archives_to_keep = local_archives[:number]
-
-        # Delete archives from the local machine that should be removed
-        for archive in local_archives[number:]:
-            local("rm -f versions/{}".format(archive))
-
-        # Delete archives on the remote servers that should be removed
-        for server in env.hosts:
-            for archive in remote_archives:
-                if archive not in archives_to_keep:
-                    run("rm -rf /data/web_static/releases/{}".format(archive))
-
-        print("Old archives removed.")
+    if number < 1:
+        number = 1
     else:
-        print("No archives to remove.")
+        number += 1  # Adding 1 to keep the most recent archive
 
-if __name__ == "__main__":
-    # Set the number of archives to keep
-    number_to_keep = 2  # Modify this number as needed
+    # Clean local versions folder
+    with lcd("versions"):
+        local("ls -t | tail -n +{} | xargs -I {{}} rm -f {{}}".format(number))
 
-    # Call the do_clean function
-    do_clean(number_to_keep)
+    # Clean remote /data/web_static/releases folders on all hosts
+    for host in env.hosts:
+        with settings(host_string=host):
+            with cd("/data/web_static/releases"):
+                run("ls -t | tail -n +{} | xargs -I {{}} rm -rf {{}}"
+                    .format(number))
+
+
+# Run the clean function with the specified number of archives to keep
+do_clean(2)
