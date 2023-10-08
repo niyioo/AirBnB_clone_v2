956c4fe5bde86adfebf4e76b7419ce681e4d190b
niyioo <niyiolagbegi@gmail.com>
2023-10-07 21:26:00 -0700

    l

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,63 +1,89 @@
-# Define a class for the web_static setup
-class web_static {
-  # Ensure that Nginx is installed and started
-  package { 'nginx':
-    ensure  => 'installed',
-  }
+# Nginx configuration file
+$nginx_conf = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
 
-  service { 'nginx':
-    ensure  => 'running',
-    enable  => true,
-  }
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
 
-  # Create directories for web_static
-  file { '/data':
-    ensure  => 'directory',
-  }
+    location /redirect_me {
+        return 301 https://www.linkedin.com/in/niyiolagbegi/;
+    }
 
-  file { '/data/web_static':
-    ensure  => 'directory',
-  }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}"
 
-  file { '/data/web_static/releases':
-    ensure  => 'directory',
-  }
+package { 'nginx':
+  ensure   => 'present',
+  provider => 'apt'
+} ->
 
-  file { '/data/web_static/shared':
-    ensure  => 'directory',
-  }
+file { '/data':
+  ensure  => 'directory'
+} ->
 
-  file { '/data/web_static/releases/test':
-    ensure  => 'directory',
-  }
+file { '/data/web_static':
+  ensure => 'directory'
+} ->
 
-  # Create an index.html file with content
-  file { '/data/web_static/releases/test/index.html':
-    ensure  => 'file',
-    content => "This is a test\n",
-  }
+file { '/data/web_static/releases':
+  ensure => 'directory'
+} ->
 
-  # Symlink current to test
-  file { '/data/web_static/current':
-    ensure  => 'link',
-    target  => '/data/web_static/releases/test',
-  }
+file { '/data/web_static/releases/test':
+  ensure => 'directory'
+} ->
 
-  # Set owner and group
-  exec { 'change_ownership':
-    command => 'chown -R ubuntu:ubuntu /data/',
-    path    => '/usr/bin',
-    onlyif  => 'test "$(stat -c %U /data/web_static)" != "ubuntu"',
-  }
+file { '/data/web_static/shared':
+  ensure => 'directory'
+} ->
 
-  # Configure Nginx site
-  file { '/etc/nginx/sites-available/default':
-    ensure  => 'file',
-    content => template('web_static/default.erb'),
-    require => Service['nginx'],
-    notify  => Service['nginx'],
-  }
+file { '/data/web_static/releases/test/index.html':
+  ensure  => 'present',
+  content => "Holberton School Puppet\n"
+} ->
+
+file { '/data/web_static/current':
+  ensure => 'link',
+  target => '/data/web_static/releases/test'
+} ->
+
+exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
 
-# Include the class to set up web_static
-include web_static
+file { '/var/www':
+  ensure => 'directory'
+} ->
+
+file { '/var/www/html':
+  ensure => 'directory'
+} ->
+
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "Holberton School Nginx\n"
+} ->
+
+file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page\n"
+} ->
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => $nginx_conf
+} ->
+
+exec { 'nginx restart':
+  path => '/etc/init.d/'
+}
