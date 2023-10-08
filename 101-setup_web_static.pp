3051b32c5d0db1614ddd816690128d11cfc39d07
niyioo <niyiolagbegi@gmail.com>
2023-10-07 21:05:01 -0700

    l

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,64 +1,63 @@
-# Update the package repository
-exec { 'apt_update':
-  command => 'apt-get update',
-  path    => '/usr/bin',
-  onlyif  => 'test -z "$(find /var/lib/apt/lists/ -maxdepth 0 -type f -name lock)"',
+# Define a class for the web_static setup
+class web_static {
+  # Ensure that Nginx is installed and started
+  package { 'nginx':
+    ensure  => 'installed',
+  }
+
+  service { 'nginx':
+    ensure  => 'running',
+    enable  => true,
+  }
+
+  # Create directories for web_static
+  file { '/data':
+    ensure  => 'directory',
+  }
+
+  file { '/data/web_static':
+    ensure  => 'directory',
+  }
+
+  file { '/data/web_static/releases':
+    ensure  => 'directory',
+  }
+
+  file { '/data/web_static/shared':
+    ensure  => 'directory',
+  }
+
+  file { '/data/web_static/releases/test':
+    ensure  => 'directory',
+  }
+
+  # Create an index.html file with content
+  file { '/data/web_static/releases/test/index.html':
+    ensure  => 'file',
+    content => "This is a test\n",
+  }
+
+  # Symlink current to test
+  file { '/data/web_static/current':
+    ensure  => 'link',
+    target  => '/data/web_static/releases/test',
+  }
+
+  # Set owner and group
+  exec { 'change_ownership':
+    command => 'chown -R ubuntu:ubuntu /data/',
+    path    => '/usr/bin',
+    onlyif  => 'test "$(stat -c %U /data/web_static)" != "ubuntu"',
+  }
+
+  # Configure Nginx site
+  file { '/etc/nginx/sites-available/default':
+    ensure  => 'file',
+    content => template('web_static/default.erb'),
+    require => Service['nginx'],
+    notify  => Service['nginx'],
+  }
 }
 
-# Upgrade installed packages
-package { 'nginx':
-  ensure  => 'installed',
-  require => Exec['apt_update'],
-}
-
-# Create web_static directories and index.html
-file { '/data/web_static/releases/test':
-  ensure => directory,
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-  mode   => '0755',
-}
-
-file { '/data/web_static/shared':
-  ensure => directory,
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-  mode   => '0755',
-}
-
-file { '/data/web_static/releases/test/index.html':
-  ensure  => file,
-  content => "This is a test\n",
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  mode    => '0644',
-}
-
-# Create a symbolic link
-file { '/data/web_static/current':
-  ensure  => link,
-  target  => '/data/web_static/releases/test',
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0755',
-}
-
-# Modify the nginx configuration
-file_line { 'hbnb_static_location':
-  path   => '/etc/nginx/sites-available/default',
-  line   => '        location /hbnb_static/ {',
-  after  => '        location / {',
-}
-
-file_line { 'alias_setting':
-  path   => '/etc/nginx/sites-available/default',
-  line   => '                alias /data/web_static/current/;',
-  after  => '        location /hbnb_static/ {',
-}
-
-# Start the nginx service
-service { 'nginx':
-  ensure     => 'running',
-  enable     => true,
-  require    => [File['/etc/nginx/sites-available/default'], File['/etc/nginx/sites-enabled/default']],
-}
+# Include the class to set up web_static
+include web_static
