fed6681db5c73446c255526847fda69c7397c71c
niyioo <niyiolagbegi@gmail.com>
2023-10-07 18:27:23 -0700

    d

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -22,27 +22,41 @@ def do_deploy(archive_path):
     Returns:
         True if all operations have been done correctly, otherwise False.
     """
-    try:
-        if not (os.path.exists(archive_path)):
-            return False
-
-        put(archive_path, '/tmp/')
-
-        timestamp = archive_path[-18:-4]
-        run('sudo mkdir -p /data/web_static/releases/web_static_{}/'.format(timestamp))
-
-        run('sudo tar -xzf /tmp/web_static_{}.tgz -C /data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
-
-        run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
-
-        run('sudo mv /data/web_static/releases/web_static_{}/web_static/* /data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
-
-        run('sudo rm -rf /data/web_static/releases/web_static_{}/web_static'.format(timestamp))
-
-        run('sudo rm -rf /data/web_static/current')
-
-        run('sudo ln -s /data/web_static/releases/web_static_{}/ /data/web_static/current'.format(timestamp))
-    except:
+    if not os.path.isfile(archive_path):
         return False
 
+    # Extract the filename and name (without extension) from the archive path
+    file = os.path.basename(archive_path)
+    name = os.path.splitext(file)[0]
+
+    # Upload the archive to /tmp/ on the web server
+    put(archive_path, "/tmp/{}".format(file))
+
+    # Remove the existing release folder
+    run("rm -rf /data/web_static/releases/{}/".format(name))
+
+    # Create the release folder
+    run("mkdir -p /data/web_static/releases/{}/".format(name))
+
+    # Uncompress the archive into the release folder
+    run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(file, name))
+
+    # Remove the uploaded archive
+    run("rm /tmp/{}".format(file))
+
+    # Move the contents of the release folder
+    run("mv /data/web_static/releases/{}/web_static/* "
+        "/data/web_static/releases/{}/".format(name, name))
+
+    # Remove the redundant web_static subfolder
+    run("rm -rf /data/web_static/releases/{}/web_static".format(name))
+
+    # Remove the existing current symlink
+    run("rm -rf /data/web_static/current")
+
+    # Create a new symlink to the latest release
+    run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+        .format(name))
+
+    print("New version deployed!")
     return True
