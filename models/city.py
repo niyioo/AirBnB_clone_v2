e49d568e361fe8623fbda6fbb5e1fd745187678b
niyioo <niyiolagbegi@gmail.com>
2023-10-20 22:41:36 +0100

    bb

--- models/city.py
+++ models/city.py
@@ -11,8 +11,7 @@ class City(BaseModel, Base):
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship('Place', backref='city',
-                          cascade='all, delete-orphan', passive_deletes=True)
+    places = relationship('Place', backref='city', cascade='all, delete-orphan', passive_deletes=True, foreign_keys='Place.city_id')
 
     def __init__(self, *args, **kwargs):
         """initializes city"""
--- models/place.py
+++ models/place.py
@@ -34,6 +34,7 @@ class Place(BaseModel, Base):
         longitude = Column(Float)
 
         # Relationships for DBStorage
+        city = relationship('City', back_populates='places', foreign_keys='Place.city_id')
         reviews = relationship('Review', backref='place', cascade='all, delete-orphan', passive_deletes=True)
         amenities = relationship('Amenity', secondary=place_amenity, viewonly=False, back_populates='place_amenities')
 
