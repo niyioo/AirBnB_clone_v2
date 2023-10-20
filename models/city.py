2bc95697ed09ab702150007190723829439cabef
niyioo <niyiolagbegi@gmail.com>
2023-10-20 13:07:30 +0100

    nn

--- models/city.py
+++ models/city.py
@@ -10,7 +10,8 @@ class City(BaseModel, Base):
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", backref="cities")
+    places = relationship('Place', backref='city', cascade='all, delete-orphan',
+                          passive_deletes=True, primaryjoin="City.id == Place.city_id")
 
     def __init__(self, *args, **kwargs):
         """initializes city"""
