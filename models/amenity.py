31434d7c083d7b91ccaa16bf0c25885450baefd6
niyioo <niyiolagbegi@gmail.com>
2023-10-22 09:52:20 +0100

    n

--- models/amenity.py
+++ models/amenity.py
@@ -3,7 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from os import getenv
+from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
