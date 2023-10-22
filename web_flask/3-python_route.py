976e141266ac5e215f231a08c32653567f77f0f0
niyioo <niyiolagbegi@gmail.com>
2023-10-22 09:17:55 +0100

    vv

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,43 +1,3 @@
-<<<<<<< HEAD
-#!/usr/bin/python3 
-""" 3. Add third view func that redirects and has default val for variable """ 
-  
-from flask import Flask 
-  
-  
-app = Flask(__name__) 
-app.url_map.strict_slashes = False 
-  
-  
-@app.route('/') 
-def hello_world(): 
-    """ Returns some text. """ 
-    return 'Hello HBNB!' 
-  
-  
-@app.route('/hbnb') 
-def hello(): 
-    """ Return other text. """ 
-    return 'HBNB' 
-  
-  
-@app.route('/c/<text>') 
-def c_text(text): 
-    """ replace text with variable. """ 
-    text = text.replace('_', ' ')
-    return 'C {}'.format(text) 
-  
-  
-@app.route('/python/') 
-@app.route('/python/<text>') 
-def python_text(text='is cool'): 
-    """ replace more text with another variable. """ 
-    text = text.replace('_', ' ') 
-    return 'Python {}'.format(text) 
-  
-  
-if __name__ == '__main__': 
-=======
 #!/usr/bin/python3
 """
 A Flask web application with four routes
@@ -78,5 +38,4 @@ def python_route(text):
 
 
 if __name__ == '__main__':
->>>>>>> 2acf3edcade1f5b022b9dcff68c9f5a12c2ebab7
     app.run(host='0.0.0.0', port=5000)
