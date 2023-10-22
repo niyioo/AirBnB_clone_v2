59d4b23d9092ecb0559d7feca42c9d20ea6c9d74
niyioo <niyiolagbegi@gmail.com>
2023-10-22 08:24:48 +0100

    nn

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-#!/usr/bin/python3 
-""" 7. Start flask service that does something. """ 
-  
-from flask import Flask 
-from flask import render_template 
-  
-  
-app = Flask(__name__) 
-app.url_map.strict_slashes = False 
-  
-  
-#@app.route() 
-  
-  
-if __name__ == '__main__': 
-    app.run(host='0.0.0.0', port=5000)
-=======
 #!/usr/bin/python3
 """
 starts a Flask web application
@@ -40,5 +22,4 @@ def teardown_db(exception):
     storage.close()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
+    app.run(host='0.0.0.0', port='5000')
