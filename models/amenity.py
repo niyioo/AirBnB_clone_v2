98b75b421b6779157f384b9743a0e04b2ee7681a
niyioo <niyiolagbegi@gmail.com>
2023-10-22 09:49:06 +0100

    nn

--- models/amenity.py
+++ models/amenity.py
@@ -8,12 +8,6 @@ from os import getenv
 
 class Amenity(BaseModel, Base):
     """ Amenity class to store Amenity information """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'amenities'
-        name = Column(String(128), nullable=False)
-    else:
-        name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes Amenity"""
-        super().__init__(*args, **kwargs)
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=place_amenity)
