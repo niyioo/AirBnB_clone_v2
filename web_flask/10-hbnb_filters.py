697b04f853f2bf19a4254993ca049585405bf5dd
niyioo <niyiolagbegi@gmail.com>
2023-10-23 06:03:17 +0100

    n

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,4 +1,4 @@
-#/usr/bin/pythonn3
+#!/usr/bin/python3
 """module 222"""
 from flask import Flask, render_template
 from models import storage
@@ -14,12 +14,16 @@ def close_session(exception):
     """Closes the SQLAlchemy session after each request."""
     storage.close()
 
+
 @app.route('/hbnb_filters', strict_slashes=False)
 def hbnb_filters():
     """Display a filtered list of States and Cities."""
-    states = sorted(list(storage.all(State).values()), key=lambda state: state.name)
+    states = sorted(list(storage.all(State).values()),
+                    key=lambda state: state.name)
     amenities = list(storage.all(Amenity).values())
-    return render_template('10-hbnb_filters.html', states=states, amenities=amenities)
+    return render_template('10-hbnb_filters.html',
+                           states=states, amenities=amenities)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -13,11 +13,14 @@ def close_session(exception):
     """Closes the SQLAlchemy session after each request."""
     storage.close()
 
+
 @app.route('/states', strict_slashes=False)
 def states():
     """Display a list of states."""
-    states = sorted(list(storage.all(State).values()), key=lambda state: state.name)
+    states = sorted(list(storage.all(State).values()),
+                    key=lambda state: state.name)
     return render_template('9-states.html', states=states)
+
 
 @app.route('/states/<id>', strict_slashes=False)
 def states_cities(id):
@@ -27,6 +30,6 @@ def states_cities(id):
         return render_template('9-states.html', state=state)
     return render_template('9-states.html', not_found=True)
 
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
-
